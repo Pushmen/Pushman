824b5d0de9eaba22fed53735ba2406654eda09cd
Brightcells <brightcells@gmail.com>
2018-01-12 14:49:36 +0800

    :fire: Removing unused codes and files.

--- pushman/oauth_settings.py
+++ pushman/oauth_settings.py
@@ -5,29 +5,40 @@ def DJANGO_WE_CFG_FUNC(request, state=None):
     """ WeChat CFG Callback Func """
 
 
+def DJANGO_WE_QUOTE_STATE_FUNC(request, state):
+    """ WeChat Quote Callback Func """
+    from utils.redis.connect import r
+    return r.quote(state)
+
+
+def DJANGO_WE_UNQUOTE_STATE_FUNC(request, state):
+    """ WeChat UnQuote Callback Func """
+    from utils.redis.connect import r
+    return r.unquote(state) or state
+
+
 def DJANGO_WE_BASE_FUNC(code, state, access_info=None):
     """ WeChat Base Redirect Callback Func """
 
 
 def DJANGO_WE_USERINFO_FUNC(code, state, access_info=None, userinfo=None):
     """ WeChat Userinfo Redirect Callback Func """
-    from account.models import UserInfo
     from django.conf import settings
     from utils.redis.connect import r
+    from utils.user.userinfo_save import userinfo_save
 
     # Save profile or something else
-    unique_identifier = userinfo.get(settings.WECHAT_UNIQUE_IDENTIFICATION, '')
+    user = userinfo_save(userinfo)
 
-    user, created = UserInfo.objects.select_for_update().get_or_create(**{settings.WECHAT_UNIQUE_IDENTIFICATION: unique_identifier})
-    user.unionid = userinfo.get('unionid', '')
-    user.openid = userinfo.get('openid', '')
-    user.nickname = userinfo.get('nickname', '')
-    user.avatar = userinfo.get('headimgurl', '')
-    user.save()
-
-    token_check_key = user.user_id
+    token_check_key = getattr(user, settings.TOKEN_CHECK_KEY)
 
     return {
         settings.TOKEN_CHECK_KEY: token_check_key,
         'vtoken': r.token(token_check_key, ex=False, buf=False),
     }
+
+
+def DJANGO_WE_SHARE_FUNC(request, state=None):
+    """ WeChat Share Callback Func """
+    # from django.conf import settings
+    # return settings.WECHAT_OAUTH2_REDIRECT_URL
--- utils/redis/rkeys.py
+++ utils/redis/rkeys.py
@@ -1,10 +1,1 @@
 # -*- coding: utf-8 -*-
-
-WEEK_SALE_INFO = 'week:sale:info:%s:%s'  # HASH， Week.thisweek().isoformat(), product_id， {uid-sale}
-WEEK_CHAMPION_INFO = 'week:champion:info:%s:%s'  # STRING， Week.thisweek().isoformat(), product_id， '{'avatar': '', 'desc': ''}'
-
-TOTAL_LEFT_BALANCE = 'total:left:balance'  # STRING，
-
-BALANCE_WITHDRAW_REQUEST_QUEUE = 'queue:pocket:request'  # 余额提现请求队列
-BALANCE_WITHDRAW_RESULT_QUEUE = 'queue:rebate:balance:withdraw:result'  # 余额提现结果队列
-BALANCE_WITHDRAW_RESULT_HASH = 'hash:rebate:balance:withdraw:result'  # HASH, 余额提现结果
